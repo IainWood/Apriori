7ee64382082aebf3e9b3d3d1fa010ae7b3dccc8b
Iain Woodburn <enjkw13@gmail.com>
2019-12-03 21:41:45 -0600

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import sys
 import copy
-from itertools import combinations, chain, groupby
+from itertools import combinations, chain
 
 def pre_process(data):
     data['open'] = ['open' if row == 1 else 'closed' for row in data['open']]
@@ -13,12 +13,10 @@ def pre_process(data):
     data['caters'] = ['caters' if row == True else 'no_caters' for row in data['caters']]
 
 def candidate_itemset_generation(L):
-    '''
-    insert into C_k
-    select p.item1, ..., p.item_m, q.item_m
-    from L_k-1 as p, L_k-1 as q
-    where p.item_1=q.item_1, ..., p.item_m-1=qitem_m-1, p.item_m < q.item_m
-    '''
+#    insert into C_k
+#    select p.item1, ..., p.item_m, q.item_m
+#    from L_k-1 as p, L_k-1 as q
+#    where p.item_1=q.item_1, ..., p.item_m-1=qitem_m-1, p.item_m < q.item_m
     
     L_copy = copy.deepcopy(L)
     p_L_copy = sorted(copy.deepcopy(L))
@@ -31,19 +29,16 @@ def candidate_itemset_generation(L):
     return prune(C_k, L_copy)
 
 def prune(candidates, L):
-    '''
-    For all itemsets c in C_k
-        For all (k-1) subsets s of c
-            If s not in L_k-1 then delete c from C_k
-    '''
+#    For all itemsets c in C_k
+#        For all (k-1) subsets s of c
+#            If s not in L_k-1 then delete c from C_k
     for c in candidates:
         for s in list(combinations(c, len(c) - 1)):
             if list(s) not in L:
                 try:
                     candidates.remove(c)
                     break
-                except:
-                    pass
+                except:pass
     return candidates
 
 def eliminate_cand(itemsets, data):
@@ -99,56 +94,36 @@ def get_freq(item):
     for t in data:
         if set(item).issubset(set(t)):
             count += 1
-    return count
+    return float(count/len(data))
 
 def rule_generation(L, freqs, minconf):
-    rules, rules_union = [], []
+    rules, rules_union, counts = [], [], []
     
     for itemset, freqset in zip(L, freqs):
+        rules = []
         for item, freq in zip(itemset, freqset):
-#            print(item)
-#            subsets = list(combinations(item, len(item) - 1))
-#            print(subsets)
-#            print(freq)
+#            subsets = chain.from_iterable(combinations(list(item), r) for r in range(1, len(list(item))))
+            for ante in chain.from_iterable(combinations(list(item), r) for r in range(1, len(list(item)))):
+                conseq = set(item) - set(ante)
+                if freq/get_freq(ante) >= minconf:
+                    rules.append(len(ante) + len(conseq))
+                    
+        rules_union.append(rules)
+
+    values = np.unique(rules_union)
+    for i in range(len(values) - 1):
+        counts.append([values[i][0], len(values[i])])
             
-            subsets = chain.from_iterable(combinations(list(item), r) for r in range(2, len(list(item))))
-#            for sub in subsets:
-#                print(sub)
-#            print(list(subsets))
-            for X in subsets:
-                
-#                print('item type: ', type(item))
-#                print('X type: ', type(X))
-#                print(X)
-                Y = set(item) - set(X)
-#                print(X, ' -> ', Y, '\tconf: ', get_freq(X)/freq)
-#                print(freq/get_freq(X))
-                if get_freq(item)/get_freq(X)>= minconf:
-                    rules.append((X, Y, len(X) + len(Y)))
-#                    print(X, ' -> ', Y, '\tlength: ', str(len(X) + len(Y)))
-#            rules_union.append(rules)
-    print(rules)
-    a_rules = []
-    for key, group in groupby():
-        a_rules.append(group)
-    return a_rules
-#    k = 1
-#    while L:
-#        H = L[0]
-#        m = 1
-#        while H:
-#            if k > m + 1:
-#                pass
-#                H = #generate rules from R_m
-#                R = #select candidates in H with minconf
-#            m += 1
-#        k += 1
+    return counts
 
 def apriori(data, minsup, minconf):
     L, freqs = frequent_itemset_generation(data, minsup)
+    for item in L:
+        print('FREQUENT-ITEMS ' + str(len(item[0])) + ' ' + str(len(item)))
         
     R = rule_generation(L, freqs, minconf)
-    return L, R
+    for item in R:
+        print('ASSOCIATION-RULES ' + str(item[0]) + ' ' + str(item[1]))
 
 if __name__ == '__main__':
     train_file = 'yelp5.csv'#sys.argv[1]
@@ -156,13 +131,7 @@ if __name__ == '__main__':
     minconf = 0.75#float(sys.argv[3])
 
     data = pd.read_csv(train_file, delimiter=',', index_col=None, engine='python')
-    data = data.head(10)
     pre_process(data)
-#    print(data)
     data = data.values
-    f_items, a_rules = apriori(data, minsup, minconf)
-
-    for item in f_items:
-        print('FREQUENT-ITEMS ' + str(len(item[0])) + ' ' + str(len(item)))
-    for item in a_rules:
-        print('ASSOCIATION-RULES ' + str(len(item[0])) + ' ' + str(len(item)))
+    apriori(data, minsup, minconf)
+    
